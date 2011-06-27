f1c06b4fb72af1e16780fbb73eab12302ac6f00f
Dave Oxley <dave@daveoxley.co.uk>
2011-06-27 11:01:40 +1000

    Add QueuJ as a scheduling and asynchronous provider.

---
+++ providers/asynchronous/queuj/src/main/java/org/jboss/seam/cron/asynchronous/queuj/QueuJAsynchronousProvider.java
@@ -0,0 +1,89 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.seam.cron.asynchronous.queuj;
+
+import com.workplacesystems.queuj.QueueFactory;
+import com.workplacesystems.queuj.process.QueujFactory;
+import com.workplacesystems.queuj.process.java.JavaProcessBuilder;
+import java.util.Locale;
+import java.util.UUID;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+
+import org.jboss.logging.Logger;
+import org.jboss.seam.cron.impl.asynchronous.exception.AsynchronousMethodInvocationException;
+import org.jboss.seam.cron.impl.scheduling.exception.CronProviderDestructionException;
+import org.jboss.seam.cron.impl.scheduling.exception.CronProviderInitialisationException;
+import org.jboss.seam.cron.spi.CronProviderLifecycle;
+import org.jboss.seam.cron.spi.asynchronous.CronAsynchronousProvider;
+import org.jboss.seam.cron.spi.asynchronous.Invoker;
+import org.jboss.seam.cron.spi.asynchronous.support.FutureInvokerSupport;
+
+/**
+ * Simple asynchronous method invocation which schedules @Asynchronous methods
+ * to be executed at some point in the very near future using the QueuJ scheduler.
+ *
+ * @author Dave Oxley
+ */
+public class QueuJAsynchronousProvider implements CronProviderLifecycle, CronAsynchronousProvider {
+
+    private static final Logger log = Logger.getLogger(QueuJAsynchronousProvider.class);
+    @Inject
+    BeanManager beanManager;
+
+    /**
+     * Initialises the scheduler.
+     *
+     */
+    public void initProvider() throws CronProviderInitialisationException {
+        try {
+            QueujFactory.getProcessServer((String)null);
+        } catch (Exception ex) {
+            throw new CronProviderInitialisationException("Error initializing QueuJ for asynchronous method invocation", ex);
+        }
+    }
+
+    /**
+     * Shutdown the scheduler on application close.
+     */
+    public void destroyProvider() throws CronProviderDestructionException {
+    }
+
+    public void executeWithoutReturn(final Invoker inkover) {
+        executeMethodAsScheduledJob(inkover);
+    }
+
+    public Future executeAndReturnFuture(final Invoker invoker) {
+        FutureTask asyncResult = new FutureTask(executeMethodAsScheduledJob(invoker));
+        new Thread(asyncResult).start();
+        return asyncResult;
+    }
+
+    private FutureInvokerSupport executeMethodAsScheduledJob(final Invoker invoker) throws AsynchronousMethodInvocationException {
+        JavaProcessBuilder jpb = QueueFactory.DEFAULT_QUEUE.newProcessBuilder(Locale.getDefault());
+        final String jobName = UUID.randomUUID().toString();
+        jpb.setProcessName(jobName);
+        final FutureInvokerSupport drs = new FutureInvokerSupport(invoker);
+        jpb.setProcessDetails(new AsyncMethodInvocationJob(), "execute", new Class[] { FutureInvokerSupport.class }, new Object[] { drs });
+        jpb.setProcessPersistence(false);
+        jpb.newProcess();
+        return drs;
+    }
+}
