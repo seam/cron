a4a315daa571e5136a68a946f334090d8d0d0f17
Pete Royle <pete@screamingcoder.com>
2015-05-06 08:18:23 +1000

    We can stop JBoss AS from hanging during deployment by using an @ApplicationScoped bean to capture the configs post bean validation, then apply those configs during a @Startup @Signleton's @PostConstruct method.

--- JBossServicePlusTimerTest/src/main/java/timertest/DummyServiceEjb.java
+++ JBossServicePlusTimerTest/src/main/java/timertest/DummyServiceEjb.java
@@ -1,20 +1,29 @@
 package timertest;
 
-import javax.ejb.LocalBean;
-//import javax.ejb.Singleton; // <!-- this causes the hang!
-import javax.inject.Singleton;
-
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.TimerService;
+import javax.inject.Inject;
 
 /**
  *
  * @author peteroyle
  */
 @Singleton
-@LocalBean
-public class DummyServiceEjb implements DummyService {
+@Startup
+public class DummyServiceEjb {
 
+    @Inject
+    private AppConfig appConfig;
+    @Resource
+    private TimerService timerService;
+
+    @PostConstruct
     public void init() {
         System.out.println("Initialising dummy service implementation");
+        System.out.println("App Config Schedule: " + appConfig.getSchedule());
     }
-    
+
 }
