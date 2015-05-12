5da9ea173294c258169c032d55fa4ec34e2e5deb
Pete Royle <pete@screamingcoder.com>
2015-05-12 13:18:04 +1000

    The HA Singleton TimerService provider should be made vendor-neutral eventually, so renamed as such.

--- providers/scheduling/timerservice-ha-singleton-jbossas/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/ejb/HATimerServiceActivator.java
+++ providers/scheduling/timerservice-ha-singleton-jbossas/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/ejb/HATimerServiceActivator.java
@@ -17,47 +17,56 @@
 package org.jboss.as.quickstarts.cluster.hasingleton.service.ejb;
 
 import org.jboss.as.clustering.singleton.SingletonService;
-import org.jboss.as.clustering.singleton.election.NamePreference;
-import org.jboss.as.clustering.singleton.election.PreferredSingletonElectionPolicy;
-import org.jboss.as.clustering.singleton.election.SimpleSingletonElectionPolicy;
 import org.jboss.logging.Logger;
 import org.jboss.msc.service.DelegatingServiceContainer;
 import org.jboss.msc.service.ServiceActivator;
 import org.jboss.msc.service.ServiceActivatorContext;
 import org.jboss.msc.service.ServiceController;
+import org.jboss.msc.service.ServiceName;
 
 /**
- * Service activator that installs the HATimerService as a clustered singleton service
- * during deployment.
+ * Service activator that installs the HATimerService as a clustered singleton service during deployment.
  *
  * @author Paul Ferraro
  * @author Wolf-Dieter Fink
  */
 public class HATimerServiceActivator implements ServiceActivator {
+
     private final Logger log = Logger.getLogger(this.getClass());
 
     @Override
     public void activate(ServiceActivatorContext context) {
-        log.info("HATimerService will be installed!");
+        log.info("JBoss HATimerServiceActivator activating. HATimerService will be installed as JBoss HA Singleton.");
 
-        HATimerService service = new HATimerService();
-        SingletonService<String> singleton = new SingletonService<String>(service, HATimerService.SINGLETON_SERVICE_NAME);
-        /*
-         * The NamePreference is a combination of the node name (-Djboss.node.name) and the name of
-         * the configured cache "singleton". If there is more than 1 node, it is possible to add more than
-         * one name and the election will use the first available node in that list.
-         *   -  To pass a chain of election policies to the singleton and tell JGroups to run the
-         * singleton on a node with a particular name, uncomment the first line  and
-         * comment the second line below.
-         *   - To pass a list of more than one node, comment the first line and uncomment the
-         * second line below.
-         */
-        singleton.setElectionPolicy(new PreferredSingletonElectionPolicy(new SimpleSingletonElectionPolicy(), new NamePreference("node1/singleton")));
-        //singleton.setElectionPolicy(new PreferredSingletonElectionPolicy(new SimpleSingletonElectionPolicy(), new NamePreference("node1/singleton"), new NamePreference("node2/singleton")));
+        log.info("Locating JBoss HA Singleton Service");
+        final ServiceController<?> haSingletonService = context.getServiceRegistry().getService(ServiceName.JBOSS.append("cluster",
+                "singleton"));
+        if (haSingletonService != null) {
+            log.info("Found JBoss HA Singleton Service");
+            HATimerService service = new HATimerService();
+            SingletonService<String> singleton = new SingletonService<String>(service, HATimerService.SINGLETON_SERVICE_NAME);
+            /*
+             * The NamePreference is a combination of the node name (-Djboss.node.name) and the name of
+             * the configured cache "singleton". If there is more than 1 node, it is possible to add more than
+             * one name and the election will use the first available node in that list.
+             *   -  To pass a chain of election policies to the singleton and tell JGroups to run the
+             * singleton on a node with a particular name, uncomment the first line  and
+             * comment the second line below.
+             *   - To pass a list of more than one node, comment the first line and uncomment the
+             * second line below.
+             */
+//            singleton.setElectionPolicy(new PreferredSingletonElectionPolicy(new SimpleSingletonElectionPolicy(), new NamePreference(
+//                    "node1/singleton")));
+//                singleton.setElectionPolicy(new PreferredSingletonElectionPolicy(new SimpleSingletonElectionPolicy(), new NamePreference("node1/singleton"), new NamePreference("node2/singleton")));
 
-        singleton.build(new DelegatingServiceContainer(context.getServiceTarget(), context.getServiceRegistry()))
-                .setInitialMode(ServiceController.Mode.ACTIVE)
-                .install()
-        ;
+            log.info("Installing HATimerService using JBoss HA Singleton Service");
+            singleton.build(new DelegatingServiceContainer(context.getServiceTarget(), context.getServiceRegistry()))
+                    .setInitialMode(ServiceController.Mode.ACTIVE)
+                    .install();
+        } else {
+            log.warn("JBoss ServiceActivator was activated, but the JBoss HA Singleton Service could not be located. "
+                    + "Cron TimerService will be deployed in NON-HA-SINGLETON mode.");
+        }
+
     }
 }
