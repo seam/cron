ceef6ee32159c864f5fa160e3d4396b849cf6f63
Pete Royle <pete@screamingcoder.com>
2015-05-12 14:06:29 +1000

    Presence of HA Singleton Service is now detected in standard timerservice provider when deployed to JBoss AS, and falls back to non-ha deployment if not found. If deployed to any other vendor the standard non-ha deployment will activate. Therefore no need for a JBoss-specific TimerService provider.

--- providers/scheduling/timerservice/src/main/java/org/jboss/seam/cron/scheduling/timerservice/singleton/jboss/HATimerService.java
+++ providers/scheduling/timerservice/src/main/java/org/jboss/seam/cron/scheduling/timerservice/singleton/jboss/HATimerService.java
@@ -7,7 +7,7 @@
  * software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under the License.
  */
-package org.jboss.as.quickstarts.cluster.hasingleton.service.ejb;
+package org.jboss.seam.cron.scheduling.timerservice.singleton.jboss;
 
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicBoolean;
